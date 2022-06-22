8724bacd6f57588cd9a7bc179e4f74e0ff418295
sAngello31 <bravoangello31@gmail.com>
2022-06-22 18:56:35 -0500

    Se registraron los metodos inicializarSistema, agregarCliente y agregarServicio

--- ProyectoPOO/src/main/java/com/mycompany/proyectopoo/ProyectoPOO.java
+++ ProyectoPOO/src/main/java/com/mycompany/proyectopoo/ProyectoPOO.java
@@ -4,7 +4,12 @@
  */
 
 package com.mycompany.proyectopoo;
+import clases.Cita;
+import clases.Servicio;
+import clases.personas.Cliente;
 import clases.personas.Empleado;
+import java.sql.Time;
+import java.util.Date;
 import java.util.Scanner;
 /**
  *
@@ -12,12 +17,36 @@ import java.util.Scanner;
  */
 public class ProyectoPOO {
     
+    //Metodo InicializarSistema
+    public static void inicializarSistema(){
+        Empleado emp1 = new Empleado(true, "1104498347", "Angello Bravo", "2687198", "adbravo@espol.edu.ec");
+        Empleado.listaEmpleados.add(emp1);
+        
+        Cliente cl1 = new Cliente("Datos", "1103323899", "Juan Perez", "2688087", "uwu@gmail.com");
+        Cliente.listaClientes.add(cl1);
+        Cliente cl2 = new Cliente("Datos2", "2303323988", "Jean Pesantes", "2687903", "xd@gmail.com");
+        Cliente.listaClientes.add(cl2);
+        
+        Servicio s1 = new Servicio("Terapia de Lenguaje", 100, 30, true);
+        Servicio.listaServicios.add(s1);
+        Servicio s2 = new Servicio("Terapia Psicopedagógica", 260, 50, true);
+        Servicio.listaServicios.add(s2);
+        
+        
+        Cita c1 = new Cita(new Date(122, 5, 3), new Time(16, 30, 0), cl1, emp1);
+        Cita.listaCitas.add(c1);
+        Cita c2 = new Cita(new Date(122, 4, 21), new Time(15, 0, 0), cl2, emp1);
+        Cita.listaCitas.add(c2);
+        
+    }
+    
     public static void main(String[] args) {
         //Variables
         Scanner sc = new Scanner(System.in);
         int i = 0; //itinerador del ciclo principal
         
         //Ciclo Principal
+        inicializarSistema();
         while(i != 6){
             System.out.println("\n\tMENU");
             System.out.println("1. Servicios");
