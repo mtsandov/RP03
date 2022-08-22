335605ccdc5df1303db5191a8d2d77de52765552
sAngello31 <bravoangello31@gmail.com>
2022-08-22 11:32:30 -0500

    Se optimizo el codigo para no tener que instanciar los 20 botones de la actividad

--- Proyecto2doParcial/src/main/java/com/mycompany/proyecto2doparcial/ActividadController.java
+++ Proyecto2doParcial/src/main/java/com/mycompany/proyecto2doparcial/ActividadController.java
@@ -40,47 +40,6 @@ public class ActividadController implements Initializable{
     private Label lbNumero;
     @FXML
     private GridPane cuadricula;
-    //Creacion de los botones
-    @FXML
-    private Button bt1;
-    @FXML
-    private Button bt2;
-    @FXML
-    private Button bt3;
-    @FXML
-    private Button bt4;
-    @FXML
-    private Button bt5;
-    @FXML
-    private Button bt6;
-    @FXML
-    private Button bt7;
-    @FXML
-    private Button bt8;
-    @FXML
-    private Button bt9;
-    @FXML
-    private Button bt10;
-    @FXML
-    private Button bt11;
-    @FXML
-    private Button bt12;
-    @FXML
-    private Button bt13;
-    @FXML
-    private Button bt14;
-    @FXML
-    private Button bt15;
-    @FXML
-    private Button bt16;
-    @FXML
-    private Button bt17;
-    @FXML
-    private Button bt18;
-    @FXML
-    private Button bt19;
-    @FXML
-    private Button bt20;
     
             
     //Atributos no FXML
@@ -134,33 +93,76 @@ public class ActividadController implements Initializable{
            Button bt = (Button)node;
            bt.setText(Integer.toString(arrNumeros.get(contador)));
            contador++;
+           
+           //Agregar el evento
+           bt.setOnAction(e -> {
+               int valorBt = Integer.valueOf(bt.getText());
+               int valorLabel = Integer.valueOf(lbNumero.getText());
+               
+               if(valorBt == valorLabel){
+                   //Eliminar valor ya seleccionado
+                   int index = arrNumeros.indexOf(valorLabel);
+                   arrNumeros.remove(index);
+                   
+                   //Cambiar el color del boton
+                   bt.setStyle("-fx-background-color: #23F751;");
+                   aciertos++;
+                   
+                   //Cambiar el numero del Label
+                   try{
+                       int aleatorio = (int) (Math.random()*arrNumeros.size());
+                       lbNumero.setText(Integer.toString(arrNumeros.get(aleatorio)));
+                   }
+                   catch(IndexOutOfBoundsException ex){
+                       System.out.println(ex.getMessage());
+                   }
+               }
+               
+               else{
+                   fallas++;
+                   System.out.println(fallas);
+               }
+               
+               if(arrNumeros.isEmpty()){
+                   iniciar = false;
+                    System.out.println("Juego acabado");
+
+                    //Guardar Informacion
+                    Cliente cl = RegistrarAtencionController.citaParaAtencion.getCliente();
+                    LocalDate fecha = RegistrarAtencionController.citaParaAtencion.getFecha();
+                    Actividad ac = new Actividad(cl, lbTiempo.getText(), fecha, "Bingo",fallas, aciertos);
+
+                    //Guardar en Texto
+                    String clString = cl.getDatos() + ";" + cl.getCedula() + ";" + cl.getNombre() + ";" + cl.getTelef() +";" + cl.getEmail();
+                    String line = clString + ";" + lbTiempo.getText() +";"+ fecha + ";" + "Bingo" +";" + fallas + ";" + aciertos + "\n";
+
+                    try(FileWriter fl = new FileWriter(App.pathActividades, true)){
+                        fl.write(line);
+                    }
+                    catch(IOException ex){
+                        System.out.println(ex.getMessage());
+                    }
+                    
+                    //Alerta
+                    Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+                    alerta.setTitle("Information Dialog");
+                    alerta.setHeaderText("Felicitaciones");
+                    alerta.setContentText("Haz completado la actividad en un tiempo de: " + lbTiempo.getText());
+                    alerta.showAndWait();
+
+                    Cita.eliminarCita(RegistrarAtencionController.citaParaAtencion);
+
+                    try{
+                        App.setRoot(App.pathFXMLCitas);
+                    }
+                    catch(IOException ex ){
+                        System.out.println(ex.getMessage());
+                    }
+               }
+           });
        }
    }
    
-   //Se llamara a los botones junto a los eventos
-   @FXML
-   private void manejarActividad() throws IOException{
-       bt1.setOnMouseClicked(new ManejadorEventos(bt1));
-       bt2.setOnMouseClicked(new ManejadorEventos(bt2));
-       bt3.setOnMouseClicked(new ManejadorEventos(bt3));
-       bt4.setOnMouseClicked(new ManejadorEventos(bt4));
-       bt5.setOnMouseClicked(new ManejadorEventos(bt5));
-       bt6.setOnMouseClicked(new ManejadorEventos(bt6));
-       bt7.setOnMouseClicked(new ManejadorEventos(bt7));
-       bt8.setOnMouseClicked(new ManejadorEventos(bt8));
-       bt9.setOnMouseClicked(new ManejadorEventos(bt9));
-       bt10.setOnMouseClicked(new ManejadorEventos(bt10));
-       bt11.setOnMouseClicked(new ManejadorEventos(bt11));
-       bt12.setOnMouseClicked(new ManejadorEventos(bt12));
-       bt13.setOnMouseClicked(new ManejadorEventos(bt13));
-       bt14.setOnMouseClicked(new ManejadorEventos(bt14));
-       bt15.setOnMouseClicked(new ManejadorEventos(bt15));
-       bt16.setOnMouseClicked(new ManejadorEventos(bt16));
-       bt17.setOnMouseClicked(new ManejadorEventos(bt17));
-       bt18.setOnMouseClicked(new ManejadorEventos(bt18));
-       bt19.setOnMouseClicked(new ManejadorEventos(bt19));
-       bt20.setOnMouseClicked(new ManejadorEventos(bt20));
-   }
     
     //Clase interna que creara un hilo para el cronometro
     private class Reloj extends Thread{
@@ -197,85 +199,6 @@ public class ActividadController implements Initializable{
             
             //System.out.println(hora + ":" + min + ":" + seg);
             Platform.runLater(() -> e.setText(hora +":" + min + ":" + seg));
-        }
-        
-    }
-    
-    //Clase interna para manejar eventos.
-    private class ManejadorEventos implements EventHandler<Event>{
-        
-        private Button btAccion;
-        
-        public ManejadorEventos(Button btAccion){
-            this.btAccion = btAccion;
-        }
-        
-        @Override
-        public void handle(Event t) {
-            int valorBt = Integer.valueOf(btAccion.getText());
-            int valorLabel = Integer.valueOf(lbNumero.getText());
-            
-            if(valorBt == valorLabel){
-                System.out.println("Ta bien :D");
-                
-                //Eliminar el valor ya seleccionado
-                int index = arrNumeros.indexOf(valorLabel);
-                arrNumeros.remove(index);
-                
-                //Cambia el color del boton
-                btAccion.setStyle("-fx-background-color: #23F751;");
-                aciertos++;
-                try{
-                    //Coloca otro numero en pantalla
-                    int aleatorio = (int) (Math.random()*arrNumeros.size());
-                    lbNumero.setText(Integer.toString(arrNumeros.get(aleatorio)));
-                }
-                catch(IndexOutOfBoundsException e){
-                    lbNumero.setText("Game Over");
-                }
-            }
-            else{
-                fallas++;
-                System.out.println(fallas);
-            }
-            
-            if(arrNumeros.isEmpty()){
-                iniciar = false;
-                System.out.println("Juego acabado");
-                
-                //Guardar Informacion
-                Cliente cl = RegistrarAtencionController.citaParaAtencion.getCliente();
-                LocalDate fecha = RegistrarAtencionController.citaParaAtencion.getFecha();
-                Actividad ac = new Actividad(cl, lbTiempo.getText(), fecha, "Bingo",fallas, aciertos);
-                
-                //Guardar en Texto
-                String clString = cl.getDatos() + ";" + cl.getCedula() + ";" + cl.getNombre() + ";" + cl.getTelef() +";" + cl.getEmail();
-                String line = clString + ";" + lbTiempo.getText() +";"+ fecha + ";" + "Bingo" +";" + fallas + ";" + aciertos + "\n";
-                
-                try(FileWriter fl = new FileWriter(App.pathActividades, true)){
-                    fl.write(line);
-                }
-                catch(IOException e){
-                    System.out.println(e.getMessage());
-                }
-                
-                //Alerta
-                Alert alerta = new Alert(Alert.AlertType.INFORMATION);
-                alerta.setTitle("Information Dialog");
-                alerta.setHeaderText("Felicitaciones");
-                alerta.setContentText("Haz completado la actividad en un tiempo de: " + lbTiempo.getText());
-                alerta.showAndWait();
-                
-                Cita.eliminarCita(RegistrarAtencionController.citaParaAtencion);
-                
-                try{
-                    App.setRoot(App.pathFXMLCitas);
-                }
-                catch(IOException e ){
-                    System.out.println(e.getMessage());
-                }
-            }
-            
         }
         
     }
